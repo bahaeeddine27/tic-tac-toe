b3c02e77a45fd798b647686d5a2066bec8487961
Bahaeeddine27 <kiliminelbooking@gmail.com>
2024-01-01 16:34:03 +0100

    first commit

---
+++ src/Square.js
@@ -0,0 +1,9 @@
+function Square(props) {
+    return (
+        <div className={'square'} {...props}>
+            {props.x ? 'x' : (props.o ? 'o' : '')}
+        </div>
+    );
+  }
+  
+  export default Square;
